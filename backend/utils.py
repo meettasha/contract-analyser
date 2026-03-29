ad7b0b9de98a612120eec16011bb78d3817ddd0c
samriddhisharma01 <64606330+samriddhisharma01@users.noreply.github.com>
2026-03-29 21:02:51 +0530

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -5,15 +5,10 @@ def extract_text(file_path):
 
     if file_path.endswith(".pdf"):
         doc = fitz.open(file_path)
-        text = ""
-
-        for page in doc:
-            text += page.get_text()
-
-        return text
+        return "".join([page.get_text() for page in doc])
 
     elif file_path.endswith(".docx"):
-
         doc = docx.Document(file_path)
-
         return "\n".join([p.text for p in doc.paragraphs])
+
+    return ""
